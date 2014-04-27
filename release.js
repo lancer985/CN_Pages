f2719573dd575d45a309e3210d5588e49c955a8d
X01X012013 <1010535820@qq.com>
2014-04-27 15:15:30 -0600

    Update release.js

--- release.js
+++ release.js
@@ -1,2 +1,21 @@
 //V0.0
-alert("脚本开发中……")
+//启动
+//
+var hyObjList = ""; //问题的list
+var hyObjRdo = ""; //答案按钮
+//
+var hyQName = ""; //问题名称
+var hyQAns = ""; //问题答案
+//核心循环
+for(i = 0; 1 < 100; 1 += 1)
+{
+  hyObjList = document.getElementById("l_" + i);
+  alert(hyObjList.innerHTML);
+};
+//结束
+
+//题库
+function hyQBank(hyQ)
+{
+  
+};
