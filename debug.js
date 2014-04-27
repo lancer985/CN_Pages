be1e7abb897496cb4f097f82f0e02f97c1f33edd
X01X012013 <1010535820@qq.com>
2014-04-27 17:11:23 -0600

    Update debug.js

--- debug.js
+++ debug.js
@@ -13,7 +13,7 @@ for(i = 0; i < 2; i += 1)
 {
   hyObjList = document.getElementById("l_" + i);
   hyQName = hyObjList.innerHTML.split(" ", 3);
-  hyQName = hyQName[1].substring(0, hyQName.length - 5)
+  hyQName = hyQName[2].substring(0, hyQName[2].length - 5)
   alert("\"" + hyQName + "\"");
 };
 //结束
