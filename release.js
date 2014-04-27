711fa82f4eeb3d5ad048e409aeabd70874e261e1
X01X012013 <1010535820@qq.com>
2014-04-27 15:22:50 -0600

    Update release.js

--- release.js
+++ release.js
@@ -11,6 +11,7 @@ for(i = 0; 1 < 100; 1 += 1)
 {
   hyObjList = document.getElementById("l_" + i);
   hyQName = hyObjList.innerHTML.split(" ");
+  hyQName = hyQName.split(chr(13));
   alert(hyQName[2]);
 };
 //结束
