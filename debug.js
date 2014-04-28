34d62c7c14b77971b7d6159376b5c5609d0faf20
X01X012013 <1010535820@qq.com>
2014-04-27 18:57:02 -0600

    Update debug.js

--- debug.js
+++ debug.js
@@ -3,8 +3,8 @@
 alert("Bilibili自动答题脚本 V0.0 \n最后更新：2014.04.27 \n按确定开始执行。 \n有可能需要很长时间，若提示脚本长时间无响应请等待。");
 //
 var hyObjList = ""; //问题的list
-var hyObjRdo = ""; //答案按钮
 //
+var hyQList = ""; //问题的object的innerHTML缓冲
 var hyQName = ""; //问题名称
 var hyQAns = ""; //问题答案
 var hyQChoice = ""; //选项名称
@@ -13,7 +13,7 @@ var hyQChoice = ""; //选项名称
 for(i = 0; i < 2; i += 1)
 {
   hyObjList = document.getElementById("l_" + i);
-  hyQName = hyObjList.innerHTML.split(">");
+  hyQList = hyObjList.innerHTML.split(">");
   hyQName = hyQName[3].split("<");
   hyQName = hyQName[0].substring(1, hyQName[0].length - 8);
   alert("\"" + hyQName + "\""); //测试
@@ -22,9 +22,7 @@ for(i = 0; i < 2; i += 1)
   {
     for(ii = 0; i < 4; i += 1)
     {
-      hyObjRdo = document.getElementById("rdo" + i + ii);
-      hyQChoice = hyObjRdo.innerHTML.split(">");
-      alert(hyQChoice); //测试
+      alert(hyQList); //测试
       hyQChoice = hyQChoice[1].split("<");
       hyQChoice = hyQChoice[0].substring(1, hyQChoice.length - 2);
       alert("\"" + hyQChoice + "\""); //测试
