481b58588b7d10cdd74f290daa78ebea83b64618
X01X012013 <1010535820@qq.com>
2014-04-27 18:45:41 -0600

    Update debug.js

--- debug.js
+++ debug.js
@@ -15,7 +15,7 @@ for(i = 0; i < 12; i += 1)
   hyObjList = document.getElementById("l_" + i);
   hyQName = hyObjList.innerHTML.split(">");
   hyQName = hyQName[3].split("<");
-  hyQName = hyQName[0].substring(0, hyQName[0].length - 8);
+  hyQName = hyQName[0].substring(0, hyQName[0].length - 10);
   alert(hyQName);
   hyQAns = hyQBank(hyQName);
   for(ii = 0; i < 4; i += 1) //读取选项
