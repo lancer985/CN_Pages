bd25b1abb0f630a83f3e116898749a1d0aac2c69
X01X012013 <1010535820@qq.com>
2014-04-29 14:53:29 -0600

    Update uncompressed.js

--- js/uncompressed.js
+++ js/uncompressed.js
@@ -1,24 +1,18 @@
 alert("Bilibili自动答题脚本 V1.4 Beta \n题库题数：18（魂淡们，赶紧帮我收集啊！） \n最后更新：2014.04.29 \n有可能需要很长时间，若提示脚本或网页长时间无响应请选择等待。 \n我们的QQ群号：204524182 \n\n按确定开始执行。");
-//变量
-var l = ""; //问题object的innerHTML
-var n = ""; //问题名称
-var a = ""; //返回的答案
-var s = ""; //选项名称
-var c = 0; //没找到计数器
-//核心循环
+var c = 0;
 try
 {
   for(i = 0; i < 100; i += 1)
   {
-    l = document.getElementById("l_" + i).split(">");
-    n = l[3].split("<");
+    var l = document.getElementById("l_" + i).split(">");
+    var n = l[3].split("<");
     n = n[0].substring(1, n[0].length - 8);
-    a = b(n);
+    var a = b(n);
     if(a != "hyErrNotFound")
     {
       for(j = 0; j < 4; j += 1)
       {
-        s = l[6 + j * 3].split("<");
+        var s = l[6 + j * 3].split("<");
         s = s[0].substring(0, s[0].length - 9);
         if(s == a)
         {
@@ -34,16 +28,11 @@ try
 }catch(e){
   alert("发生错误，脚本将终止。 \n若您在答题页面运行的这个脚本，请截图并发给我。 \n错误信息： \n" + e);
 };
-//
-//题库
 function b(q)
 {
   switch(q)
   {
-//-----题库开始-----
 
-//-----题库结束-----
-//若没找到
     default: return "hyErrNotFound";
   };
 };
