95fa24cd83cbfa53158c1574df0601126b01da22
X01X012013 <1010535820@qq.com>
2014-04-29 14:37:43 -0600

    Update release.js

--- release.js
+++ release.js
@@ -1,47 +1,11 @@
 alert("Bilibili自动答题脚本 V1.4Beta \n题库题数：18（魂淡们，赶紧帮我收集啊！） \n最后更新：2014.04.29 \n有可能需要很长时间，若提示脚本或网页长时间无响应请选择等待。 \n我们的QQ群号：204524182 \n\n按确定开始执行。");
-//变量
-var l = ""; //问题object的innerHTML
-var n = ""; //问题名称
-var a = ""; //返回的答案
-var s = ""; //选项名称
-var c = 0; //没找到计数器
-//核心循环
-try
-{
-  for(i = 0; i < 100; i += 1)
-  {
-    l = document.getElementById("l_" + i).split(">");
-    n = l[3].split("<");
-    n = n[0].substring(1, n[0].length - 8);
-    a = b(n);
-    if(a != "hyErrNotFound")
-    {
-      for(j = 0; j < 4; j += 1)
-      {
-        s = l[6 + j * 3].split("<");
-        s = s[0].substring(0, s[0].length - 9);
-        if(s == a)
-        {
-          $('#rdo' + i + j).attr('checked', 'checked');
-          break;
-        };
-      };
-    }else{
-      c += 1;
-    };
-  };
-  alert("自动答题结束，有" + c + "道题没找到…… \n请您检查一遍后提交（如果发现题库有错误请告诉我；如果脚本太给力还是等二十分钟再提交，答题太快有可能被封号呢）。 \n欢迎您加入我们的讨论群一起讨论完善题库！ \n我们的QQ群号：204524182");
-}catch(e){
-  alert("发生错误，脚本将终止。 \n若您在答题页面运行的这个脚本，请截图并发给我。 \n错误信息： \n" + e);
-};
-//
-//题库
-function b(q)
-{
-switch(q)
-{
+var l = "";var n = "";var a = "";var s = "";var c = 0;
+try{for(i = 0; i < 100; i += 1){l = document.getElementById("l_" + i).split(">");n = l[3].split("<");n = n[0].substring(1, n[0].length - 8);a = b(n);if(a != "hyErrNotFound"){
+for(j = 0; j < 4; j += 1){s = l[6 + j * 3].split("<");s = s[0].substring(0, s[0].length - 9);if(s == a){$('#rdo' + i + j).attr('checked', 'checked');break;};};}else{c += 1;
+};};alert("自动答题结束，有" + c + "道题没找到…… \n请您检查一遍后提交（如果发现题库有错误请告诉我；如果脚本太给力还是等二十分钟再提交，答题太快有可能被封号呢）。 \n欢迎您加入我们的讨论群一起讨论完善题库！ \n我们的QQ群号：204524182");
+}catch(e){alert("发生错误，脚本将终止。 \n若您在答题页面运行的这个脚本，请截图并发给我。 \n错误信息： \n" + e);};
+function b(q){switch(q){
 //-----题库开始-----
-
 //18道 - 【群主】？（1043201997）提供 - 2014.04.29
 case "Bilibili采用邀请码注册，注册满几个月以上的用户才能购买邀请码？": return "三个月";
 case "自古红蓝出什么？": return "CP";
@@ -61,9 +25,5 @@ case "地球距离月球之间的距离为？": return "384\,400千米";
 case "网络上11区指的是那个国家？": return "日本";
 case "日语\“我爱你\”的空耳是什么？": return "阿姨洗铁路";
 case "《某科学的超电磁炮》中，佐天同学经常对初春同学做的一件事情是什么？": return "掀裙";
-
 //-----题库结束-----
-//若没找到
-default: return "hyErrNotFound";
-};
-};
+default: return "hyErrNotFound";};};
