bebcf9fc5617088d30c6b774d097410cb3e14a68
X01X012013 <1010535820@qq.com>
2014-05-01 15:17:17 -0600

    Update launcher.js

--- release/launcher.js
+++ release/launcher.js
@@ -1,5 +1,5 @@
 var o = document.createElement("script");
 o.setAttribute("type","text\/javascript");
-o.setAttribute("src", "http://X01X012013.github.io/BiliMacro/js/release.js");
+o.setAttribute("src", "http://X01X012013.github.io/BiliMacro/release/core.js");
 o.setAttribute("charset", "UTF-8");
 document.getElementsByTagName("head")[0].appendChild(o);
