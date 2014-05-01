e8611432ce426ecfd678f1bec934a4307b5b3205
X01X012013 <1010535820@qq.com>
2014-05-01 15:28:11 -0600

    Update launcher.js

--- release/launcher.js
+++ release/launcher.js
@@ -1,4 +1,4 @@
-alert("点击确定开始加载，有可能需要加载一段时间，请不要重复执行。");
+alert("点击确定开始加载，有可能需要加载一段时间，请不要重复执行。 \n若使用谷歌浏览器，请不要勾选“禁止此页再显示对话框。”。");
 var o = document.createElement("script");
 o.setAttribute("type","text\/javascript");
 o.setAttribute("src", "http://X01X012013.github.io/BiliMacro/release/core.js");
