614803ab8d07b9e646966e196cd9dcd2a6525cde
X01X012013 <1010535820@qq.com>
2014-05-01 15:07:43 -0600

    Update and rename launcher_uncompress.js to launcher.js

---
+++ release/launcher.js
@@ -0,0 +1,5 @@
+var o = document.createElement("script");
+o.setAttribute("type","text\/javascript");
+o.setAttribute("src", "http://X01X012013.github.io/BiliMacro/js/release.js");
+o.setAttribute("charset", "UTF-8");
+document.getElementsByTagName("head")[0].appendChild(o);
