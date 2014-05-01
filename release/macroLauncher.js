300d02c6c9d93918e2ecb3c878cf88523877dab0
X01X012013 <1010535820@qq.com>
2014-05-01 17:03:19 -0600

    Update macroLauncher.js

--- release/macroLauncher.js
+++ release/macroLauncher.js
@@ -1,19 +1,6 @@
 alert("点击确定开始加载，有可能需要加载一段时间，请不要重复执行。 \n若使用谷歌浏览器，请不要勾选“禁止此页再显示对话框。”。");
-var file = "http://X01X012013.github.io/BiliMacro/release/macroCore.js";
-var all = t("script");
-for(var i = all.length; i >= 0; i -= 1)
-{
-  if(all[i] && all[i].getAttribute("src")!=null && all[i].getAttribute("src").indexOf("core.js") != -1)
-  {
-   all[i].parentNode.removeChild(all[i]);
-  };
-};
-var obj = document.createElement("script");
-obj.setAttribute("type","text\/javascript");
-obj.setAttribute("src", file);
-obj.setAttribute("charset", "UTF-8");
-t("head")[0].appendChild(obj);
-function t(id)
-{
-  return document.getElementsByTagName(id)
-};
+var o = document.createElement("script");
+o.setAttribute("type","text\/javascript");
+o.setAttribute("src", "http://X01X012013.github.io/BiliMacro/release/core.js");
+o.setAttribute("charset", "UTF-8");
+document.getElementsByTagName("head")[0].appendChild(o);
