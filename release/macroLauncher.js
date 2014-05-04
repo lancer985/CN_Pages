c918b64009dcb372ade5240c7639a4d970d8409a
X01X012013 <1010535820@qq.com>
2014-05-04 07:28:44 -0600

    Delete macroLauncher.js

--- release/macroLauncher.js
+++
@@ -1,6 +0,0 @@
-alert("点击确定开始加载，有可能需要加载一段时间，请不要重复执行。");
-var o = document.createElement("script");
-o.setAttribute("type","text/javascript");
-o.setAttribute("src", "http://X01X012013.github.io/BiliMacro/release/macroCore.js");
-o.setAttribute("charset", "utf-8");
-document.getElementsByTagName("head")[0].appendChild(o);
