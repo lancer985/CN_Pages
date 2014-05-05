25cc35ec6e282d91f17d5938d28c23d2d25fdfc2
X01X012013 <1010535820@qq.com>
2014-05-05 17:05:58 -0600

    Update and rename macroCoreIE.js to macroCore.js

--- release/macroCore.js
+++ release/macroCore.js
@@ -1,8 +1,14 @@
-var v = "Bilibili自动答题脚本（Internet Explorer 11专用） V2.3";
+var v = "Bilibili自动答题脚本 V3.0";
 p = document.createElement("script");
 p.setAttribute('src', 'http://X01X012013.github.io/BiliMacro/release/macroBank.js');
 p.setAttribute('charset', 'utf-8');
 document.getElementsByTagName('head')[0].appendChild(p);
+if(r == "Internet Explorer 11")
+{
+  g = 6;
+}else{
+  g = 7;
+};
 function k()
 {
   var c = 0;
@@ -27,7 +33,7 @@ function k()
         {
           if(j == 4)
           {
-            alert("题库错误，若您使用的是Internet Explorer 11浏览器，请截图并发给我。 \n\n问题：\"" + n + "\" \n返回的答案：\"" + a + "\" \n最后一次判定调用：\"" + s + "\" \n缓存：\"" + l + "\"");
+            alert("题库错误，若您使用的是" + r + "浏览器，请截图并发给我。 \n\n问题：\"" + n + "\" \n返回的答案：\"" + a + "\" \n最后一次判定调用：\"" + s + "\" \n缓存：\"" + l + "\"");
             if(f)
             {
               z(i, 0);
@@ -35,7 +41,7 @@ function k()
             d += 1;
             break;
           };
-          var s = l[6 + j * 3].split("<");
+          var s = l[g + j * 3].split("<");
           s = s[0].substring(0, s[0].length - 9);
           if(s == a)
           {
@@ -53,7 +59,7 @@ function k()
     };
     alert("自动答题结束，有" + c + "道题没找到，有" + d + "道题返回无效（题库错误）…… \n请您检查一遍后提交（如果发现题库有错误请告诉我；如果脚本太给力还是等二十分钟再提交，答题太快有可能被封号呢）。 \n欢迎您加入我们的讨论群一起讨论完善题库！ \n我们的QQ群号：204524182");
   }catch(e){
-    alert("发生错误，脚本将终止。 \n若您在答题页面运行的这个脚本且您的浏览器是Internet Explorer 11，请截图并发给我。 \n错误信息： \n" + e);
+    alert("发生错误，脚本将终止。 \n若您在答题页面运行的这个脚本且您的浏览器是" + r + "，请截图并发给我。 \n错误信息： \n" + e);
   };
   t();
 };
