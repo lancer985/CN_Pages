43bac4b90f51e2e4f456bb417bab7b608d500dc9
X01X012013 <1010535820@qq.com>
2014-11-22 12:41:11 -0700

    Update index.js

--- Bili_Macro/index.js
+++ Bili_Macro/index.js
@@ -1,19 +1,4 @@
-//script for index.html by X01X012013
-//-----on startup-----
-//关闭保护
-window.onbeforeunload = function(hyEvent) 
-{
-  var hyUnloadMSG = "您真的要离开么？";
-  if(typeof hyEvent == "undefined")
-  {
-    hyEvent = window.event;
-  };
-  if(hyEvent)
-  {
-    hyEvent.returnValue = hyUnloadMSG;
-  };
-  return hyUnloadMSG;
-};
+
 
 var hyRowCount = 0;
 var hyBuffer = "";
@@ -46,10 +31,10 @@ function hyLS(hyName, hyCMD, hyData)
   };
 };
 
-function hyTableH(hyCMD)
+var table = function(c)
 {
-  hyTB = "<input type='text' class='hyNoMar' style='width:98%' "
-  if(hyCMD == "add")
+  hyTB = "<input type=\"text\" "
+  if(c === "add")
   {
     for(var i = 0; i < 10; i += 1)
     {
@@ -80,3 +65,35 @@ function hyTableH(hyCMD)
     hyID('hyOP').focus();
   };
 };
+/*main*/
+window.onload = function(){
+  /*init*/
+  table("add");
+  $("#genOutput").val("按“生成”按钮生成，生成的数据将出现在这里。\n生成时会覆盖这里原有的数据。\n\n您可以点击“增加十行”来在表格的末尾插入十行空白表格行。\n您可以点击“清空”来重置表格的所有行。\n\n“保存”可以把这里的数据存到浏览器的Local Storage中；您以后可以点击“读取”按钮读取（正常情况下，若您不手动清理您浏览器的浏览数据，Local Storage中的数据永远不会消失）。");
+  /*add*/
+  $("#bAdd10").onclick(function(){
+    table("add");
+  });
+  /*del*/
+  $("#bClear").onclick(function(){
+    table("del");
+  });
+  $("#bGen").onclick(function(){
+    table("gen");
+  });
+  $("#bLSSave").onclick(function(){
+    if(confirm('您真的要保存输出框中的数据么？ \n这将覆盖Local Storage中原有的数据。')){
+      hyLS('hyBiliMacroSave', 'w', hyID('hyOP').value);
+      alert('已将输出框中的数据保存到Local Storage。');
+    }
+  });
+  $("#bLSLoad").onclick(function(){
+    if(confirm('您真的要读取保存的输出数据么？ \n这将覆盖输出框中原有的数据。')){
+      hyID('hyOP').value = hyLS('hyBiliMacroSave', 'r');
+      alert('已从Local Storage读取输出数据。');
+    }
+  });
+}
+window.onbeforeunload = function(){
+  return "您真的要离开么？";
+}
