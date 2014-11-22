50f142e91de31e3d6a66ab2945de55c22adf3731
X01X012013 <1010535820@qq.com>
2014-11-22 15:55:26 -0700

    Update index.js

--- Bili_Macro/index.js
+++ Bili_Macro/index.js
@@ -1,3 +1,4 @@
+var defaultTable = "<tr><th id=\"genTableQ\">问题</th><th id=\"genTableA\">答案</th></tr>";
 var tableRowCount = 0;
 /*main*/
 window.onload = function(){
@@ -14,7 +15,7 @@ window.onload = function(){
     return null;
   }
   /*init*/
-  $("#genTable").html("<tr><th id=\"genTableQ\">问题</th><th id=\"genTableA\">答案</th></tr>");
+  $("#genTable").html(defaultTable);
   tableAdd();
   $("#genOutput").val("按“生成”按钮生成，生成的数据将出现在这里。\n生成时会覆盖这里原有的数据。\n\n您可以点击“增加十行”来在表格的末尾插入十行空白表格行。\n您可以点击“清空”来重置表格的所有行。\n\n“保存”可以把这里的数据存到浏览器的Local Storage中；您以后可以点击“读取”按钮读取（正常情况下，若您不手动清理您浏览器的浏览数据，Local Storage中的数据永远不会消失）。");
   /*add 10 rows*/
@@ -22,7 +23,7 @@ window.onload = function(){
   /*clear the table*/
   $("#bClear").click(function(){
     if(confirm("您真的想重置表格么？")){
-      $("#genTable").html("<tr><th id=\"genTableQ\">问题</th><th id=\"genTableA\">答案</th></tr>");
+      $("#genTable").html(defaultTable);
     }
     tableRowCount = 0;
     return null;
