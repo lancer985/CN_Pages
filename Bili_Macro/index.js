48ffc6ef74b96e23b0beb2782f752ae129489a15
X01X012013 <1010535820@qq.com>
2014-11-22 13:48:48 -0700

    Update index.js

--- Bili_Macro/index.js
+++ Bili_Macro/index.js
@@ -18,9 +18,9 @@ window.onload = function(){
   tableAdd();
   $("#genOutput").val("按“生成”按钮生成，生成的数据将出现在这里。\n生成时会覆盖这里原有的数据。\n\n您可以点击“增加十行”来在表格的末尾插入十行空白表格行。\n您可以点击“清空”来重置表格的所有行。\n\n“保存”可以把这里的数据存到浏览器的Local Storage中；您以后可以点击“读取”按钮读取（正常情况下，若您不手动清理您浏览器的浏览数据，Local Storage中的数据永远不会消失）。");
   /*add 10 rows*/
-  $("#bAdd10").onclick(tableAdd);
+  $("#bAdd10").click(tableAdd);
   /*clear the table*/
-  $("#bClear").onclick(function(){
+  $("#bClear").click(function(){
     if(confirm("您真的想重置表格么？")){
       $("#genTable").html("<tr><th id=\"genTableQ\">问题</th><th id=\"genTableA\">答案</th></tr>");
     }
@@ -28,7 +28,7 @@ window.onload = function(){
     return null;
   });
   /*generate*/
-  $("#bGen").onclick(function(){
+  $("#bGen").click(function(){
     var buffer = "";
     for(var i = 0; i < tableRowCount; i++ ){
       buffer += "case \"" + $("#genTextBox" + i + "0").val() + "\": return \"" +  $("#genTextBox" + i + "1").val() + "\";\n";
@@ -37,7 +37,7 @@ window.onload = function(){
     return null;
   });
   /*save to local storage*/
-  $("#bLSSave").onclick(function(){
+  $("#bLSSave").click(function(){
     if(confirm('您真的要保存输出框中的数据么？ \n这将覆盖已保存的数据。')){
       try{
         localStorage.setItem("BilibiliMacroSave", $("#genOutput").val());
@@ -48,7 +48,7 @@ window.onload = function(){
     return null;
   });
   /*load from local storage*/
-  $("#bLSLoad").onclick(function(){
+  $("#bLSLoad").click(function(){
     if(confirm('您真的要读取保存的输出数据么？ \n这将覆盖输出框中原有的数据。')){
       try{
         $("#genOutput").val(localStorage.getItem("BilibiliMacroSave"));
