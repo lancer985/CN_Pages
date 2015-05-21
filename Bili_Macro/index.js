b89f236fd5a05b9ce8024bbc65697bca0ffac5db
X01X012013 <1010535820@qq.com>
2015-05-20 20:09:01 -0600

    Update index.js

--- Bili_Macro/index.js
+++ Bili_Macro/index.js
@@ -17,7 +17,7 @@ window.onload = function(){
   /*init*/
   $("#genTable").html(defaultTable);
   tableAdd();
-  $("#genOutput").val("按“生成”按钮生成，生成的数据将出现在这里。\n生成时会覆盖这里原有的数据。\n\n您可以点击“增加十行”来在表格的末尾插入十行空白表格行。\n您可以点击“清空”来重置表格的所有行。\n\n“保存”可以把这里的数据存到浏览器的Local Storage中；您以后可以点击“读取”按钮读取（正常情况下，若您不手动清理您浏览器的浏览数据，Local Storage中的数据永远不会消失）。");
+  $("#genOutput").val("加载完成。");
   /*add 10 rows*/
   $("#bAdd10").click(tableAdd);
   /*clear the table*/
