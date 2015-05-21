3b3503a7054d30f912db51a1718f77b966e25a3b
X01X012013 <1010535820@qq.com>
2015-05-20 19:15:29 -0600

    Update macro.js

--- Bili_Macro/new_beta/macro.js
+++ Bili_Macro/new_beta/macro.js
@@ -1,17 +1,42 @@
 //欢迎
 alert("脚本内测中，请舔屏幕继续。");
 //核心
+//将全部20题存到一个Array
 var allQ = [];
 $(".examLi").each(function(){
-    //将全部20题存到一个Array
     allQ.push($(this).text());
 });
+//提取单个题的问题和选项
 for(var i = 1; i <= 20; i++){
-    //提取单个题的问题和选项
-    var q = "", a = []; //初始化变量
     var thisQ = allQ[i].split(" ");
-    q = thisQ[64].substring(0, thisQ[64].length - 2); //问题 index 64
-    for(var ii = 112; i <= 172; i += 20){ //第一个答案的index是112，之后每20一个答案，一共四个答案
-        a.push(thisQ[i].substring(2, thisQ[i].length - 2);
+    var q = thisQ[64].substring(0, thisQ[64].length - 2); //问题 index 64
+    var a = [];
+    for(var ii = 112; ii <= 172; ii += 20){ //第一个答案的index是112，之后每20一个答案，一共四个答案
+        a.push(thisQ[i].substring(2, thisQ[i].length - 2));
     }
+    var rightA = getAns(q); //获取答案
+    for(var iii = 0; iii <= 4; iii++){
+        //如果答案找不到就跳过
+        if(rightA === undefined){
+            break;
+        }
+        //如果返回的答案不存在就提醒用户报告给我
+        if(iii === 4){
+            alert("系统错误，请截图并发给我！\n问题：\"" + q + "\"\n正确答案：\"" + rightA + "\"\n答案列表：" + a);
+            break;
+        }
+        //填上答案
+        if(rightA === a[iii]){
+            selAns();
+            break;
+        }
+    }
+}
+//题库函数 - 制作中
+function getAns (q){
+    return "";
+}
+//填空函数 - 制作中
+function selAns (){
+    return "";
 }
