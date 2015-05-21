dde6407000ee8b368190ad665ebf1c124478e8e6
X01X012013 <1010535820@qq.com>
2015-05-20 18:59:52 -0600

    Update macro.js

--- Bili_Macro/new_beta/macro.js
+++ Bili_Macro/new_beta/macro.js
@@ -1,12 +1,15 @@
 //欢迎
 alert("脚本内测中，请舔屏幕继续。");
 //核心
-$.each($(".examLi"), function(index, data){
-    if(index === 0){
-        //第一个是空的
-        return undefined;
-    }
-    //处理开始
-    var tempList = data.split(">");
-    console.log(tempList);
-})
+var allQ = [];
+$(".examLi").each(function(){
+    //将全部20题存到一个Array
+    allQ.push($(this).text());
+});
+for(var i = 1;i <= 21; i++){
+    //提取单个题的问题和选项
+    var q = "", a = []; //初始化变量
+    var thisQ = allQ[i].split(" ");
+    q = thisQ[64].substring(0, thisQ[64].length - 2);
+    alert(q)
+}
