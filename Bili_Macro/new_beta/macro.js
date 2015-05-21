fcc4925514d7df369a54209762d77d350dee8049
X01X012013 <1010535820@qq.com>
2015-05-20 18:38:10 -0600

    Update macro.js

--- Bili_Macro/new_beta/macro.js
+++ Bili_Macro/new_beta/macro.js
@@ -1,6 +1,12 @@
+//欢迎
 alert("脚本内测中，请舔屏幕继续。");
+//核心
 $.each($(".examLi"), function(index, data){
-    console.log(index);
-    console.log(data);
-  }
-)
+    if(index === 0){
+        //第一个是空的
+        return undefined;
+    }
+    //处理开始
+    var tempList = data.split(">");
+    console.log(tempList);
+})
