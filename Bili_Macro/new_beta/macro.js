c378c89f255f04ca0d73e5a5f4b321281bebb434
X01X012013 <1010535820@qq.com>
2015-05-20 19:37:43 -0600

    Update macro.js

--- Bili_Macro/new_beta/macro.js
+++ Bili_Macro/new_beta/macro.js
@@ -10,11 +10,15 @@ $(".examLi").each(function(){
 for(var i = 1; i <= 20; i++){
     var thisQ = allQ[i].split(" ");
     var q = thisQ[64].substring(0, thisQ[64].length - 2); //问题 index 64
+    /*
     var a = [];
     for(var ii = 112; ii <= 172; ii += 20){ //第一个答案的index是112，之后每20一个答案，一共四个答案
         a.push(thisQ[i].substring(2, thisQ[i].length - 2));
     }
+    */
     var rightA = getAns(q); //获取答案
+    $(".examLi").eq(i).find("li:contains('" + rightA + "')").addClass("currSolution");
+    /*
     for(var iii = 0; iii <= 4; iii++){
         //如果答案找不到就跳过
         if(rightA === undefined){
@@ -31,6 +35,7 @@ for(var i = 1; i <= 20; i++){
             break;
         }
     }
+    */
 }
 //题库函数 - 制作中
 function getAns (q){
