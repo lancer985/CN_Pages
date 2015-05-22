049cc70594b8b73685891690fa6151213717a563
X01X012013 <1010535820@qq.com>
2015-05-21 19:26:16 -0600

    Update part1.js

--- Bili_Macro/release/part1.js
+++ Bili_Macro/release/part1.js
@@ -5,7 +5,7 @@ V1.0 2015.05.20
 V1.1 2015.05.21
 1.大量细节调整
 */
-try{ //错误处理
+try{ //系统错误处理
 //欢迎和运行确认
 if(prompt("Bilibili晋级考试 弹幕礼仪部分 答题脚本 V1.1（2015.05.21更新）\n点击确定运行（点击取消则不会运行）。\n我们的网站（包含脚本使用说明，使用本脚本前请务必阅读）：", "http://x01x012013.github.io/CN_Pages/Bili_Macro/") !== null){
 alert("题库题数：2（2015.05.20更新）\n若您为使用此脚本支付了任何费用，那么恭喜您，您被坑了。\n点击确定继续。");
@@ -27,10 +27,12 @@ for(var i = 1; i <= 20; i++){
         $(".examLi").eq(i).find("li:contains('" + rightA + "')").addClass("currSolution");
     }else{
         counter += 1;
-        alert("题库错误！\n问题：\"" + q + "\"\n返回的答案：\"" + rightA + "\"\n捕获的答案列表：" + a);
+        if(rightA !== undefined){ //题库错误处理
+            alert("题库错误！\n问题：\"" + q + "\"\n返回的答案：\"" + rightA + "\"\n捕获的答案列表：" + a);
+        }
     }
 }
-alert("答题完成，有" + counter  +"道题没有找到。")
+alert("答题完成，有" + counter  +"道题没填写（题库中没有或返回的答案无效）。")
 //题库函数
 function getAns (q){
 // 2 + 0 = 2
@@ -38,10 +40,10 @@ switch(q){
 //群主提供 2题 2015.05.20
 case "下列哪一条不是剧透弹幕？": return ".我非常喜欢这个角色";
 case "以下哪种弹幕不会被禁言？": return ".和谐吐槽";
-//没找到的话返回undefined
+//
 default: return undefined;
 }}
 }//结束运行确认
-}catch(e){ //错误处理
+}catch(e){ //系统错误处理
     alert("系统错误！\n错误信息：\n" + e);
 }
